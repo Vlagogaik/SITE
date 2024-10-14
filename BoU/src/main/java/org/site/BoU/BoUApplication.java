e2a97be5f8df905eff55b4366f9e092d29a2593a
Vlagogaik <100479b@mail.ru>
2024-10-14 13:16:11 +0300

    create spring

---
+++ BoU/src/main/java/org/site/BoU/BoUApplication.java
@@ -0,0 +1,13 @@
+package org.site.BoU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoUApplication.class, args);
+	}
+
+}
---
+++ BoU/src/test/java/org/site/BoU/BoUApplicationTests.java
@@ -0,0 +1,13 @@
+package org.site.BoU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
